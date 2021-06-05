b96caecc722a62f09b982d5c9f87b0545cceceae
Sibyl <sibyl@wormbase.org>
2021-06-05 15:28:42 -0400

    configure tailwind

--- apps/ui/src/main.js
+++ apps/ui/src/main.js
@@ -2,8 +2,11 @@ import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './app/App';
+import { GlobalStyles } from 'twin.macro';
+
 ReactDOM.render(
   <StrictMode>
+    <GlobalStyles />
     <BrowserRouter>
       <App />
     </BrowserRouter>
