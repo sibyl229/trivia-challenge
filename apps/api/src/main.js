fa424983603a573b6818602a20ca52b204747766
Sibyl <sibyl@wormbase.org>
2021-06-05 13:34:00 -0400

    start new round

--- apps/api/src/main.js
+++ apps/api/src/main.js
@@ -11,6 +11,62 @@ app.get('/api', (req, res) => {
   res.send({ message: 'Welcome to api!' });
 });
 
+const QUESTION_BANK = [
+  {
+    category: 'Sports',
+    question: 'What’s the diameter of a basketball hoop in inches?',
+    choices: {
+      a: '14 inches',
+      b: '16 inches',
+      c: '18 inches'
+    },
+    answer: 'c',
+  },
+  {
+    category: 'Science',
+    question: 'This essential gas is important so that we can breathe',
+    choices: {
+      a: 'Oxygen',
+      b: 'Nitrogen',
+      c: 'Helium',
+    },
+    answer: 'a',
+  }
+].map((element, index) => ({
+  ...element,
+  score: Math.round(Math.random() * 10) * 100
+})),
+
+var currentRound = {
+  questions: [],
+};
+
+/**
+ * Shuffles array in place. ES6 version
+ * @param {Array} a items An array containing the items.
+ */
+ function shuffle(a) {
+  for (let i = a.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [a[i], a[j]] = [a[j], a[i]];
+  }
+  return a;
+}
+
+app.get('/api/new-round', (req, res) => {
+  const selectedQuestions = shuffle([...QUESTION_BANK]).slice(0, 16);
+  currentRound = {
+    questions: selectedQuestions,
+  }
+  res.send({
+    questions: selectedQuestions.map(({category, score}, index) => ({
+      id: index,
+      category,
+      score,
+    }))
+  })
+})
+
 const port = process.env.port || 3333;
 const server = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/api`);
